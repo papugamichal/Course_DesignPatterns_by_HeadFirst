2d97e2e19bd3fdb53aba89fea26b1fd6b40d79db
Michał Papuga <papuga.michal@hotmail.com>
2021-02-17 20:57:35 +0100

    Add project files.

---
+++ 6_Command/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
